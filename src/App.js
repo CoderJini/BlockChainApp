9aa14bfc76ca161643b42727e3a691e9e74099cc
CoderJini <ksjer2182@naver.com>
2022-07-04 11:37:29 +0900

    [2022/07/04] 페이지 및 라우트 구성

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import "./App.css";
 import Header from "./component/Header";
+import Dashboard from "./component/Dashboard";
+import PageContent from "./component/PageContent";
+import SettingContent from "./component/SettingContent";
+import NotFoundPage from "./component/NotFoundPage";
+
+import {BrowserRouter, Route, Routes, Link} from "react-router-dom"
 
 function App() {
 
   return (
-    <div>
+    <BrowserRouter>
       <Header/>
-    </div>
+      <Routes>
+        {/* 메인페이지 (대시보드) */}
+        <Route path="/" element={<Dashboard/>}/>
+        {/* 메인페이지 (SamplePage 복사용) */}
+        <Route path="/page" element={<PageContent/>}/>
+        {/* 설정페이지 */}
+        <Route path="/setting" element={<SettingContent/>}/>
+        {/* 페이지를 찾을수 없습니다. */}
+        <Route path="*" element={<NotFoundPage/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- src/component/Header.js
+++ src/component/Header.js
@@ -1,18 +1,22 @@
 import styles from "./Header.module.css";
 import {useState} from "react";
+import dummy from "../db/dummy.json";
+import {BrowserRouter, Route, Routes, Link} from "react-router-dom"
 
-const HeaderComponent = () =>{
+const Header = () =>{
 
     return (
         <header className="navbar navbar-inverse">
-            <a class="navbar-brand">Block Chain App</a>
-            <ul class="nav navbar-nav navbar-left">
-                <li><a href="#">대시보드</a></li>
-                <li><a href="#">페이지1</a></li>
-                <li><a href="#">설정</a></li>
+            <Link to="/" className="navbar-brand">Block Chain App</Link>
+            <ul className="nav navbar-nav navbar-left">
+                {
+                    dummy.mainManu.map(mainManu=>(
+                        <li key={mainManu.id}><Link to={mainManu.router}>{mainManu.name}</Link></li>
+                    ))
+                }
             </ul>
         </header>
     )
 }
 
-export default HeaderComponent;
+export default Header;
