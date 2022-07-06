f7e56b4511cdd447bf0a9d42477d4dbdfe1e866b
CoderJini <ksjer2182@naver.com>
2022-07-06 10:18:04 +0900

    [2022/07/06] 클레이튼샘플 Count App 화면 추가

--- src/component/klaytnSampleApp/KlayCountApp.js
+++ src/component/klaytnSampleApp/KlayCountApp.js
@@ -11,7 +11,37 @@ const KlayCountApp = () =>{
 
     return (
         <Fragment>
-            <button onClick={onTest}>test</button>
+            {/* col-xs-12 col-sm-6 col-md-8 */}
+            <div className="row"> 
+                <div className="
+                    col-md-10 col-sm-10 col-xs-10 
+                    col-md-offset-1 col-sm-offset-1 col-xs-offset-1">
+                    <div className="panel panel-default">
+                        <div className="panel-heading">
+                            <h3 className="panel-title">Klaytn Countting App </h3>
+                        </div>
+                        <div className="panel-body">
+                            <p className="lead">버튼을 클릭하여 count를 증가 또는 감소 시켜보세요!!</p>
+                            <p className="lead">Count Result is : </p>
+                            {/* count 올리기 */}
+                            <button 
+                                type="button" 
+                                className="btn btn-default" 
+                                onClick={onTest}>
+                                <span className="glyphicon glyphicon-menu-up" aria-hidden="true"></span>
+                            </button>
+
+                            {/* count 내리기 */}
+                            <button 
+                                type="button" 
+                                className="btn btn-default" 
+                                onClick={onTest}>
+                                <span className="glyphicon glyphicon-menu-down" aria-hidden="true"></span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </Fragment>
     )
 }
